e95744e1c433793d0c72bf665264a80cce73249e
Mark Gannaway <mark@ganners.co.uk>
2016-08-11 17:07:03 +0100

    Initial work on the client

---
+++ cli-client/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/Ganners/yoti-test/encryption-server/pb/encryption"
+	"github.com/ganners/gossip"
+	"github.com/ganners/yoti-test/client"
+	"github.com/gogo/protobuf/proto"
+)
+
+func main() {
+	// Create a new gossip server
+	server, err := gossip.NewServer(
+		"cli-client",
+		"The client which launches syncronous client requests",
+		"0.0.0.0", "8003",
+		gossip.NewSilentLogger(),
+	)
+
+	// Launch a new client
+
+	<-server.Start()
+	log.Println("Terminating client server")
+}
+
+var (
+	// Make sure CLIClient implements client.Client
+	_ client.Client = &CLIClient{}
+)
+
+// CLIClient takes a Gossip server and will broadcast and wait for responses
+// back
+type CLIClient struct {
+	server *gossip.Server
+}
+
+// NewCLIClient simply returns a new CLIClient - pass in a gossip server that
+// will launch
+func NewCLIClient(server *gossip.Server) *CLIClient {
+	return &CLIClient{
+		server: server,
+	}
+}
+
+// Implements the Store part of the interface
+func (cli *CLIClient) Store(id, payload []byte) (aesKey []byte, err error) {
+	// Execute a request and wait for response back
+	writeRequest := &encryption.EncryptedWriteRequest{
+		Id:        id,
+		Plaintext: payload,
+	}
+	err, resp, timeout := cli.server.BroadcastAndWaitForResponse(
+		"encryption-server.write",
+		writeRequest)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to broadcast retrieve: %s", err)
+	}
+
+	select {
+	case <-timeout:
+		return nil, errors.New("timed out waiting for retrieve response")
+	case responseEnvelope := <-resp:
+		writeResp := &encryption.EncryptedWriteResponse{}
+		err := proto.Unmarshal(responseEnvelope.EncodedMessage, writeResp)
+		if err != nil {
+			return fmt.Errorf("unable to unmarshal write request: %+v", err)
+		}
+
+		// Return the text
+		return writeResp.Key, nil
+	}
+
+	return nil, nil
+}
+
+// Implements the Retrieve part of the interface
+func (cli *CLIClient) Retrieve(id, aesKey []byte) (payload []byte, err error) {
+	// Execute a request and wait for response back
+	readRequest := &encryption.EncryptedReadRequest{
+		Key: aesKey,
+		Id:  id,
+	}
+	err, resp, timeout := cli.server.BroadcastAndWaitForResponse(
+		"encryption-server.read",
+		readRequest)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to broadcast retrieve: %s", err)
+	}
+
+	select {
+	case <-timeout:
+		return nil, errors.New("timed out waiting for retrieve response")
+	case responseEnvelope := <-resp:
+		readRsp := &encryption.EncryptedReadResponse{}
+		err := proto.Unmarshal(responseEnvelope.EncodedMessage, readRsp)
+		if err != nil {
+			return fmt.Errorf("unable to unmarshal read request: %+v", err)
+		}
+
+		// Return the text
+		return readRsp.Plaintext, nil
+	}
+
+	return nil, nil
+}
